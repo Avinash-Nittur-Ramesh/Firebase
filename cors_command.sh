27430aa90d23ac8691beaa4e71dbfd885143491b
Avinash <rameshavinashnittur@gmail.com>
2022-02-08 20:39:31 +0100

    Learnt basics of accessing Google Cloud bucket

---
+++ cors_command.sh
@@ -0,0 +1,5 @@
+curl --request PATCH \
+ 'https://storage.googleapis.com/storage/v1/b/learning-812cb.appspot.com?fields=cors' \
+ --header 'Authorization: Bearer ya29.A0ARrdaM8myk87QGEvQhS_lRQD-FuDyDPdEO6BfwMouCqm_bP60wX8RLXwbhulfN_Q7tmGZEWpLXapdLRLJKr_yoVfQh2-tmgutRmDN9A1E7U19xMx9kO7dtGQmFRQzHjgX9Wp3DRNRcQYFORAhKZMtEv383nJ' \
+ --header 'Content-Type: application/json' \
+ --data-binary @cors-config.json
